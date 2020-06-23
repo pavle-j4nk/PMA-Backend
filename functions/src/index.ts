6810ad6d96055fffbee43f510cdf37c400769bfb
Jovana Grabez <jovanagrabez@uns.ac.rs>
2020-06-23 23:16:54 +0200

    sendNotification function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,15 +1,15 @@
 'use strict';
 
 import * as functions from 'firebase-functions';
-import { firestore, initializeApp } from 'firebase-admin';
+import { firestore, initializeApp, messaging } from 'firebase-admin';
 import { CrudService } from './crudService';
 
 const MOCK_USER = false
 const COLLECTION_OFFER = 'offer'
 const COLLECTION_RESERVATION = 'reservation'
 const COLLECTION_NOTIFICATION_TOKEN = 'notificationToken'
+const COLLECTION_NOTIFICATION = 'notification'
 const COLLECTION_USER = 'reservation'
-
 initializeApp();
 
 const db: firestore.Firestore = firestore()
@@ -29,7 +29,32 @@ const getAll = (collectionName: string) =>
 const get = (collectionName: string) =>
     (request: functions.https.Request, response: functions.Response<any>) =>
         crudService.getById(collectionName, request.params[0].substr(1), (data) =>
-            data ? response.send(data) : response.status(404).send({ error: "Not Found" }))
+            data ? response.send(data) : response.status(404).send({ error: "Not Found" }));
+
+export const sendNotification = functions.https.onCall( (data,context) => {
+   
+   //for testing use token of yours device
+    var registrationToken= data.userToken;
+    var message = {
+        notification: {
+          title: data.titel,
+          body: 'You recive notification from user' + data.userName
+        },
+         token: registrationToken
+      };
+      
+      // Send a message to devices subscribed to the combination of topics
+      // specified by the provided condition.
+      messaging().send(message)
+        .then((response) => {
+          // Response is a message ID string.
+          console.log('Successfully sent message:', response);
+        })
+        .catch((error) => {
+          console.log('Error sending message:', error);
+        });
+});
+
 
 export const createOffer = functions.https.onCall(create(COLLECTION_OFFER))
 export const updateOffer = functions.https.onCall(update(COLLECTION_OFFER));
@@ -50,3 +75,8 @@ export const createUser = functions.https.onCall(create(COLLECTION_USER))
 export const updateUser = functions.https.onCall(update(COLLECTION_USER));
 export const getUsers = functions.https.onRequest(getAll(COLLECTION_USER));
 export const getUser = functions.https.onRequest(get(COLLECTION_USER));
+
+export const createNotification = functions.https.onCall(create(COLLECTION_NOTIFICATION))
+export const updateNotification = functions.https.onCall(update(COLLECTION_NOTIFICATION));
+export const getNotifications = functions.https.onRequest(getAll(COLLECTION_NOTIFICATION));
+export const getNotification = functions.https.onRequest(get(COLLECTION_NOTIFICATION));
